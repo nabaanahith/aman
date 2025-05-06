56b5ed82bceba763d39edc556f880e7ecd23e736
nabaanahith <42347859+nabaanahith@users.noreply.github.com>
2025-05-06 17:30:59 +0300

    Update hero-section.tsx

--- src/pages/components/hero-section.tsx
+++ src/pages/components/hero-section.tsx
@@ -2,8 +2,7 @@
 
 export default function HeroSection() {
   const [mounted, setMounted] = useState(false);
-  const { language } = useLanguageStore();
-  const localizedData = getLocalizedContent(language);
+
 
   useEffect(() => {
     setMounted(true);
