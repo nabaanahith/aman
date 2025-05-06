7d2c13f243f5b51b2bef2e21041034496609a719
nabaanahith <42347859+nabaanahith@users.noreply.github.com>
2025-05-06 17:33:18 +0300

    Update hero-section.tsx

--- src/pages/components/hero-section.tsx
+++ src/pages/components/hero-section.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 export default function HeroSection() {
-  const [mounted, setMounted] = useState(false);
 
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
-  if (!mounted) return null;
 
   return (
    <section className="relative rounded-xl  ">
