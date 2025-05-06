46ab549a8a9d5c622028482fd1a928e26f36c872
nabaanahith <42347859+nabaanahith@users.noreply.github.com>
2025-05-06 17:29:47 +0300

    Update hero-section.tsx

--- src/pages/components/hero-section.tsx
+++ src/pages/components/hero-section.tsx
@@ -1,13 +1,4 @@
 "use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-import Illustrations from "./Illustrations";
-import  Navbar  from "./navbar";
-import {
-  getLocalizedContent,
-  useLanguageStore,
-} from "../../app/store/useLanguageStore";
-import CheckoutButton from "./paymentInput";
 
 export default function HeroSection() {
   const [mounted, setMounted] = useState(false);
