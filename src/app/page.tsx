5b678630d459bea804a0c352778756662d4ff16d
nabaanahith <42347859+nabaanahith@users.noreply.github.com>
2025-05-07 13:06:45 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,6 @@ import StatisticsSection from "../pages/components/statistics-section";
 import VideoMessageSection from "../pages/components/video-secction";
 
 export default function Home() {
-  return ()
+
    
 }
