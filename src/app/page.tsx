42d3622e77e3c276f9a611cb5b83d3fb5d97b1fe
nabaanahith <42347859+nabaanahith@users.noreply.github.com>
2025-05-07 13:07:28 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import HeroSection from "../pages/components/hero-section";
-import FooterSection from "../pages/components/footer-section";
-import StatisticsSection from "../pages/components/statistics-section";
-import VideoMessageSection from "../pages/components/video-secction";
+
 
 export default function Home() {
 
